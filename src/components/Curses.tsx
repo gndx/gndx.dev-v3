0d51655e4d3ab3f7531cc423ee59f0859e6be1a0
Oscar Barajas <oscar@gndx.dev>
2022-10-16 22:55:26 -0500

    :zap: [UPDATE] courses

--- src/components/Curses.tsx
+++ src/components/Curses.tsx
@@ -19,30 +19,41 @@ interface CourseDataProps {
 const Courses = ({ max, random }: CoursesProps) => {
 	const courses = random ? randomPosts(coursesData) : coursesData;
 	return (
-		<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+		<div className="mt-8 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-2">
 			{courses?.slice(0, max).map((course: CourseDataProps) => (
-				<a
-					className="relative block overflow-hidden rounded-lg border bg-gray-200 p-8"
-					href={course.url}
-					target="_blank"
-				>
-					<span className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
-
-					<div className="justify-between sm:flex">
-						<div>
-							<h5 className="h-24 text-xl font-bold text-gray-900">
-								{course?.title}
-							</h5>
-						</div>
-						<div className="ml-3 hidden flex-shrink-0 sm:block">
-							<img
-								className="h-16 w-16 rounded-lg object-cover shadow-sm"
-								src={course?.badge}
-								alt={course?.title as string}
-							/>
+				<div className="text-[0.8125rem] leading-5">
+					<div className="flex items-center">
+						<img
+							src={course?.badge}
+							alt={course?.title as string}
+							className="h-10 w-10 flex-none rounded-full"
+						/>
+						<div className="ml-4 flex-auto">
+							<div className="w-52 font-medium">
+								<a href={course?.url} target="_blank">
+									{course?.title}
+								</a>
+							</div>
+							<div className="mt-1 flex items-center text-slate-500">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									className="mr-2 h-4 w-4"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+									/>
+								</svg>
+								{course?.reviews}
+							</div>
 						</div>
 					</div>
-				</a>
+				</div>
 			))}
 		</div>
 	);
