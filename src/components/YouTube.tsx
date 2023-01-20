d7861a9112008b25db5d66d2713504ef6a7f94e7
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-01-20 11:01:58 +0000

    Add YT videos info

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -1,26 +1,33 @@
 import { PlayIcon } from "@heroicons/react/24/solid/index.js";
+import youtubeData from "../data/youtubeData";
 
 const YouTube = () => {
 	return (
-		<article>
-			<a
-				href="https://www.youtube.com/channel/UCw05fUBPwmpu-ehXFMqfdMw?sub_confirmation=1"
-				target={"_blank"}
-				className="relative block overflow-hidden rounded-xl bg-[url(https://arepa.s3.amazonaws.com/oscar-barajas-gndx.png)] bg-cover bg-center bg-no-repeat"
-			>
-				<span className="absolute right-4 top-4 z-10 inline-flex items-center rounded-xl bg-red-600 px-4 py-2 text-xs font-semibold text-white">
-					<PlayIcon className="ml-1.5 h-6 w-6 text-white" />
-				</span>
+		youtubeData.slice(0,2).map((v => 
 
-				<div className="relative bg-black bg-opacity-40 p-8 pt-32 text-white">
-					<h5 className="mt-2 text-2xl font-bold">Oscar Barajas @gndx</h5>
-					<p className="mt-2 text-sm text-gray-400">Último Video:</p>
-					<p className="text-sm">
-						Desde la Historia a WEB3 | ¿Que es Web 3.0? ¿Cómo funciona?
-					</p>
-				</div>
-			</a>
-		</article>
+
+		<article>
+		<a href={v.url}
+			target={"_blank"}
+			 className="relative top-0 block overflow-hidden rounded-2xl">
+			<img
+				alt={v.title}
+				src={v.thumbnail}
+				className="h-44 w-full object-cover"
+			/>
+			<div className="bg-gray-900 p-4">
+				<p className="mb-1 text-sm text-gray-400">Video</p>
+				<h5 className="text-sm text-white">{v.title}</h5>
+				<p className="mt-1 text-xs text-gray-400">
+				{v.description}	
+				</p>
+			</div>
+		</a>
+	</article>
+			
+			
+			)
+		)
 	);
 };
 
---
+++ src/data/youtubeData.ts
@@ -0,0 +1,95 @@
+const youtubeData = [
+
+    {
+        title: "Cosas que te hacen dejar un trabajo",
+        thumbnail: "https://i1.ytimg.com/vi/0kNkLZo28UU/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=0kNkLZo28UU",
+        description: "➤ Conviértete en miembro: https://www.youtube.com/thatcsharpguy/join",
+    },
+    {
+        title: "el MEJOR libro para comenzar con machine learning",
+        thumbnail: "https://i1.ytimg.com/vi/XbeI97iMbb8/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=XbeI97iMbb8",
+        description: "",
+    },
+    {
+        title: "Cosas curiosas en Reino Unido",
+        thumbnail: "https://i3.ytimg.com/vi/fLTWOeGUU-k/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=fLTWOeGUU-k",
+        description: "http://www.bbctvlicence.com/",
+    },
+    {
+        title: "Applied Text analysis with Python está más o menos",
+        thumbnail: "https://i2.ytimg.com/vi/1SYIHUIeX0g/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=1SYIHUIeX0g",
+        description: "",
+    },
+    {
+        title: "Envía mensajes de WhatsApp desde Python",
+        thumbnail: "https://i4.ytimg.com/vi/CHSQxXibuCQ/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=CHSQxXibuCQ",
+        description: "Crea tu cuenta de Twilio: https://www.twilio.com/try-twilio?utm_source=social&utm_medium=influencers&utm_campaign=developerslatam&utm_term=antonioferegrino",
+    },
+    {
+        title: "Lo MEJOR de NOVIEMBRE 2022",
+        thumbnail: "https://i3.ytimg.com/vi/jqBmBmFyqGM/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=jqBmBmFyqGM",
+        description: "Consider Disabling Browser Push Notifications on Family and Friends Devices https://www.lloydatkinson.net/posts/2022/consider-disabling-browser-push-notifications-on-family-and-friends-devices/",
+    },
+    {
+        title: "Entrevista con Lupe Canaviri",
+        thumbnail: "https://i3.ytimg.com/vi/2jdsOXlPeIA/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=2jdsOXlPeIA",
+        description: "Me acompaña Lupe, desarrolladora front end en Microsoft",
+    },
+    {
+        title: "6 preguntas que SIEMPRE me hacen sobre CIENCIA de DATOS",
+        thumbnail: "https://i1.ytimg.com/vi/DDxlnMnT7Z0/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=DDxlnMnT7Z0",
+        description: "Aquí te respondo unas preguntas que siempre me hacen sobre mi vida en la ciencia de datos.",
+    },
+    {
+        title: "¿Por qué le gustó la ciencia de datos a @yesidays?",
+        thumbnail: "https://i4.ytimg.com/vi/OV1UHBGjhwQ/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=OV1UHBGjhwQ",
+        description: "Ve la entrevista completa, acá: https://youtu.be/ek3ATGZ8n_Q",
+    },
+    {
+        title: "¿Quieres trabajar en Reino Unido? – TIPS",
+        thumbnail: "https://i3.ytimg.com/vi/vOvzV_z3bug/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=vOvzV_z3bug",
+        description: "Te traigo algunos consejos para que te animes a buscar trabajo en Reino Unido",
+    },
+    {
+        title: "Entrevista con Charliesbot – Desarrollando para YouTube",
+        thumbnail: "https://i3.ytimg.com/vi/zMEY4oFB5PU/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=zMEY4oFB5PU",
+        description: "Me acompaña Charlie, para hablar de su carrera y YouTube .",
+    },
+    {
+        title: "Las MEJORES fuentes para PROGRAMAR",
+        thumbnail: "https://i3.ytimg.com/vi/R1nDBVClBxU/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=R1nDBVClBxU",
+        description: "Fira Code: https://github.com/tonsky/FiraCode",
+    },
+    {
+        title: "¿Qué es programar de acuerdo a Eva Navarro?",
+        thumbnail: "https://i1.ytimg.com/vi/d6e-RrwbnDw/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=d6e-RrwbnDw",
+        description: "",
+    },
+    {
+        title: "Unboxing de Keychrone K8 y qué está pasando en Twitter",
+        thumbnail: "https://i3.ytimg.com/vi/bWEng11C0HA/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=bWEng11C0HA",
+        description: "Hola mundo",
+    },
+    {
+        title: "The Hundred-Page Machine Learning Book 👌",
+        thumbnail: "https://i3.ytimg.com/vi/6g69TDr41HM/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=6g69TDr41HM",
+        description: "",
+    },
+];
+
+export default youtubeData;
